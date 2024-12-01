9c7b0680c52d7df4f68be1055809c43e687bec0f
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-12-01 13:53:13 +0100

    test-data

---
+++ src/comp/inform/Information.jsx
@@ -0,0 +1,31 @@
+// T-SHIRTS
+
+import tShirt1 from "../../img/t-shirts/t-shirt 1.png";
+
+// SWEATERS
+
+// JEANS
+
+// BAGS
+
+export const productsT = [
+  { id: 1, type: "t-shirt", price: 25, image: tShirt1 },
+  { id: 2, type: "t-shirt", price: 20, image: tShirt1 },
+  { id: 3, type: "t-shirt", price: 28, image: tShirt1 },
+  { id: 4, type: "t-shirt", price: 15, image: tShirt1 },
+];
+
+// export const productsS = [
+//   { id: 1, type: "sweater", price: 45, image: sweater1 },
+//   { id: 2, type: "sweater", price: 50, image: sweater1 },
+// ];
+
+// export const productsJ = [
+//   { id: 1, type: "jeans", price: 60, image: jeans1 },
+//   { id: 2, type: "jeans", price: 55, image: jeans1 },
+// ];
+
+// export const productsB = [
+//   { id: 1, type: "bag", price: 35, image: bag1 },
+//   { id: 2, type: "bag", price: 40, image: bag1 },
+// ];
